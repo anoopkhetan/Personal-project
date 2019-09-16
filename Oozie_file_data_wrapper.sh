f550d2bfb186d69d6517e8870149662164e7933c
anoopkhetan <khetan.anoop@gmail.com>
2019-09-15 23:00:44 -0400

    Delete Oozie_file_data_wrapper.sh

--- Oozie_file_data_wrapper.sh
+++
@@ -1,6 +0,0 @@
-export FLAGS="-Djavax.net.ssl.trustStore=/usr/local/admin/uat2_certs/gdch01-cluster02.truststore"
-
-export OOZIE_URL=https://gdch01d13.statestr.com:11443/oozie
-
-oozie $FLAGS job $OOZIE_URL -config ../properties/file_data-dev.properties -run
-
